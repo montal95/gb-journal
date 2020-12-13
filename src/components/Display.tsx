a6bce2ef02c42fc3ded1ed4783c005f5db14a3e1
Sam M <sammontalvojr@gmail.com>
2020-12-13 16:04:34 -0600

    added service worker and logo images

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from 'react'
+import React, { useContext } from 'react'
 import { GlobalContext } from './GlobalContext'
 import {GhostInfo} from './GhostInfo'
 import { PageHeaders } from './elements'
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './Global'
+import registerServiceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,9 +12,10 @@ ReactDOM.render(
     <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
+  );
+  
+  // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+registerServiceWorker();
 reportWebVitals();
